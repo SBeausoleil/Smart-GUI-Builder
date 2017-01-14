bafaf5faf6e9d9d1a1c79da0a0d774f8a828d71b
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2017-01-14 19:42:41 +0100

    Addition of a test for primitives

--- src/com/sb/smartgui/test/TestClassUtilMain.java
+++ src/com/sb/smartgui/test/TestClassUtilMain.java
@@ -6,5 +6,7 @@ public class TestClassUtilMain {
     public static void main(String[] args) {
 	TestRootObject obj = ClassUtil.instantiate(TestRootObject.class);
 	System.out.println(obj.toString());
+	int integer = ClassUtil.instantiate(int.class);
+	System.out.println(integer);
     }
 }
